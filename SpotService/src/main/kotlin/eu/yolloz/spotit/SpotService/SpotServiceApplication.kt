23387ae2207f6ee908341b0f97fc158fe0861e51
Michal Jurco <michal.jurco007@gmail.com>
2023-12-16 12:54:37 +0100

    Basic project setup

---
+++ SpotService/src/main/kotlin/eu/yolloz/spotit/SpotService/SpotServiceApplication.kt
@@ -0,0 +1,11 @@
+package eu.yolloz.spotit.SpotService
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotServiceApplication>(*args)
+}
---
+++ SpotService/src/test/kotlin/eu/yolloz/spotit/SpotService/SpotServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package eu.yolloz.spotit.SpotService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
